243eaa96244e1fdb89391b820faf293f01be364b
Dominic Nguyen <ddnguyen@student.fullsail.edu>
2020-02-18 19:58:46 -0800

    Set up routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
-import { HashRouter, Switch, Link} from 'react-router-dom'
+import { HashRouter, Switch, Route} from 'react-router-dom'
 
 import Home from './components/Home'
+import Header from './components/Header'
+import About from './components/About'
+import Members from './components/Members'
+import Schedule from './components/Schedule'
+import Contact from './components/Contact'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 // Hashrouter hashes url
 class App extends Component {
   render() {
     return (
       <HashRouter>
-        <Switch>
-            <Link exact to ="/" component={Home}/>
-        </Switch>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/members" component={Members}/>
+            <Route path="/schedule" component={Schedule}/>
+            <Route path="/contact" component={Contact}/>
+          </Switch>
       </HashRouter>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 
+import { Nav, Navbar, NavbarBrand, NavItem, NavLink, Container } from "reactstrap"
+
 class Header extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <Navbar color="light" light expand="md">
+                <Container>
+                    <Nav className="mr-auto" navbar>
+                        <NavbarBrand href="#/">Bella Bella Bella</NavbarBrand>
+                        <NavItem>
+                            <NavLink href="#/">Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#/about">About</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#/members">Members</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#/schedule">Schedule</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#/contact">Contact</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Container>
+            </Navbar>
         );
     }
 }
