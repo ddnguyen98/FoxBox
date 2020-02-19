3c5d3a994d0c5c6d9e94bf51b639d41f117db176
Dominic Nguyen <ddnguyen@student.fullsail.edu>
2020-02-18 16:20:42 -0800

    Testing

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                Dogs on the daily
+            </div>
+        );
+    }
+}
+
+export default Home;
